57d6a23ba414046913b9b8050eeb57eb84a50a66
Kawtious <mewmewby2@gmail.com>
2022-11-28 16:37:40 -0700

    Finally got the motivation to optimize everything

--- Scripts/Data/Firebase.cs
+++ Scripts/Data/Firebase.cs
@@ -1,97 +1,88 @@
 using Godot;
-using Godot.Collections;
 using System.Text;
+
+using Array = Godot.Collections.Array;
 using Dictionary = Godot.Collections.Dictionary;
 
 public class Firebase : Node
 {
 
-    public HTTPRequest POSTRequest;
-
-    public HTTPRequest PUTRequest;
-
-    public HTTPRequest GETRequest;
-
-    public HTTPRequest UpdateDishesRequest;
-
-    public HTTPRequest MondayMenuRequest;
-
-    public HTTPRequest TuedayMenuRequest;
-
-    public HTTPRequest WeddayMenuRequest;
-
-    public HTTPRequest ThudayMenuRequest;
-
-    public HTTPRequest FridayMenuRequest;
-
-    [Signal]
-    delegate void UpdatedDishList();
-
-    [Signal]
-    delegate void UpdatedMondayMenu();
-
-    [Signal]
-    delegate void UpdatedTuesdayMenu();
-
-    [Signal]
-    delegate void UpdatedWednesdayMenu();
-
-    [Signal]
-    delegate void UpdatedThursdayMenu();
-
-    [Signal]
-    delegate void UpdatedFridayMenu();
-
-    public readonly string[] Headers = new string[] { "Content-Type: application/json" };
+    public const string DATABASE_REFERENCE = "https://kiawa-service-default-rtdb.firebaseio.com/data";
 
     public const string JSON_EXTENSION = ".json";
 
-    public const string DATABASE_REFERENCE = "https://kiawa-service-default-rtdb.firebaseio.com/data/";
+    public HTTPRequest SetMenuRequest;
 
-    public const string DISH_REFERENCE = DATABASE_REFERENCE + "dishes/";
+    public HTTPRequest DataRequest;
 
-    public const string MENU_REFERENCE = DATABASE_REFERENCE + "menu/";
+    [Signal]
+    delegate void UpdatedData();
 
-    public const string
-            MON_REFERENCE = "monday/",
-            TUE_REFERENCE = "tuesday/",
-            WED_REFERENCE = "wednesday/",
-            THU_REFERENCE = "thursday/",
-            FRI_REFERENCE = "friday/";
+    private Dictionary Data = new Dictionary();
 
-    public Array Dishes = new Array();
+    public Dictionary Menu
+    {
+        get { return Data["menu"] as Dictionary; }
+    }
 
-    public Array MondayMenu = new Array(),
-            TuesdayMenu = new Array(),
-            WednesdayMenu = new Array(),
-            ThursdayMenu = new Array(),
-            FridayMenu = new Array();
+    public Array Dishes
+    {
+        get { return Data["dishes"] as Array; }
+    }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         InitNodes();
-        //MakeGETRequest();
-        //GD.Print(TestMenu[1]);
     }
 
     private void InitNodes()
     {
-        POSTRequest = GetNode<HTTPRequest>("POSTRequest");
-        PUTRequest = GetNode<HTTPRequest>("PUTRequest");
-        GETRequest = GetNode<HTTPRequest>("GETRequest");
-        UpdateDishesRequest = GetNode<HTTPRequest>("UpdateDishesRequest");
-
-        MondayMenuRequest = GetNode<HTTPRequest>("MondayMenuRequest");
-        TuedayMenuRequest = GetNode<HTTPRequest>("TuedayMenuRequest");
-        WeddayMenuRequest = GetNode<HTTPRequest>("WeddayMenuRequest");
-        ThudayMenuRequest = GetNode<HTTPRequest>("ThudayMenuRequest");
-        FridayMenuRequest = GetNode<HTTPRequest>("FridayMenuRequest");
+        SetMenuRequest = GetNode<HTTPRequest>("SetMenuRequest");
+        DataRequest = GetNode<HTTPRequest>("DataRequest");
     }
 
-    public void UpdateDishes()
+    public Array GetMenu(string day)
     {
-        Error error = UpdateDishesRequest.Request(DISH_REFERENCE + JSON_EXTENSION);
+        Array result = new Array();
+
+        if (Menu.Contains(day.ToLower()))
+        {
+            result = Menu[day.ToLower()] as Array;
+        }
+
+        return result;
+    }
+
+    public Dictionary GetDish(int index)
+    {
+        return Dishes[index] as Dictionary;
+    }
+
+    public Array GetTodayMenu()
+    {
+        string today = System.DateTime.Now.DayOfWeek.ToString();
+        return GetMenu(today);
+    }
+
+    public void SetMenu(string day, Array dishes)
+    {
+        string reference = Firebase.DATABASE_REFERENCE +
+                        "/menu/" +
+                        day.ToLower() +
+                        Firebase.JSON_EXTENSION;
+
+        PUTRequest(reference, dishes);
+    }
+
+    public void UpdateData()
+    {
+        APIRequest(DataRequest, DATABASE_REFERENCE);
+    }
+
+    private void APIRequest(HTTPRequest node, string target)
+    {
+        Error error = node.Request(target + JSON_EXTENSION);
 
         if (error != Error.Ok)
         {
@@ -99,31 +90,12 @@ public class Firebase : Node
         }
     }
 
-    public void UpdateMenu(HTTPRequest request, string day)
+    public void PUTRequest(string reference, object data_to_send)
     {
-        Error error = request.Request(MENU_REFERENCE + day + JSON_EXTENSION);
-
-        if (error != Error.Ok)
-        {
-            GD.PrintErr("Request failed.");
-        }
-    }
-
-    private void MakeGETRequest(string reference)
-    {
-        Error error = GETRequest.Request(reference);
-
-        if (error != Error.Ok)
-        {
-            GD.PrintErr("Request failed.");
-        }
-    }
-
-    public void MakePOSTRequest(string reference, object data_to_send)
-    {
+        string[] headers = new string[] { "Content-Type: application/json" };
         string query = JSON.Print(data_to_send);
 
-        Error error = POSTRequest.Request(reference, Headers, false, HTTPClient.Method.Post, query);
+        Error error = SetMenuRequest.Request(reference, headers, false, HTTPClient.Method.Put, query);
 
         if (error != Error.Ok)
         {
@@ -131,121 +103,20 @@ public class Firebase : Node
         }
     }
 
-    public void MakePUTRequest(string reference, object data_to_send)
+    public void _OnDataRequestCompleted(int result, int response_code, string[] headers, byte[] body)
     {
-        string query = JSON.Print(data_to_send);
+        JSONParseResult json = JSON.Parse(Encoding.UTF8.GetString(body));
+        object _result = json.Result;
 
-        Error error = PUTRequest.Request(reference, Headers, false, HTTPClient.Method.Put, query);
-
-        if (error != Error.Ok)
+        if (_result != null)
         {
-            GD.PrintErr("Request failed.");
+            if (_result is Dictionary)
+            {
+                Data = _result as Dictionary;
+            }
+
+            EmitSignal("UpdatedData");
         }
-    }
-
-    public void _OnGETRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-    {
-        JSONParseResult json = JSON.Parse(Encoding.UTF8.GetString(body));
-        object Result = json.Result;
-
-        GD.Print(Result);
-
-        Dictionary Results = Result as Dictionary;
-    }
-
-    public void _OnPUTRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-    {
-    }
-
-    public void _OnPOSTRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-    {
-    }
-
-    public void _OnUpdateDishesRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-    {
-        JSONParseResult json = JSON.Parse(Encoding.UTF8.GetString(body));
-        object Result = json.Result;
-
-        GD.Print(Result);
-
-        Array Results = Result as Array;
-        GD.Print(Results);
-
-        if (Results != null)
-        {
-            Dishes = Results;
-            EmitSignal("UpdatedDishList");
-        }
-    }
-
-    public void _OnMondayMenuRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-    {
-        JSONParseResult json = JSON.Parse(Encoding.UTF8.GetString(body));
-        object Result = json.Result;
-
-        GD.Print(Result);
-
-        Array Results = Result as Array;
-        GD.Print(Results);
-
-        MondayMenu = Results;
-        EmitSignal("UpdatedMondayMenu");
-    }
-
-    public void _OnTuedayMenuRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-    {
-        JSONParseResult json = JSON.Parse(Encoding.UTF8.GetString(body));
-        object Result = json.Result;
-
-        GD.Print(Result);
-
-        Array Results = Result as Array;
-        GD.Print(Results);
-
-        TuesdayMenu = Results;
-        EmitSignal("UpdatedTuesdayMenu");
-    }
-
-    public void _OnWeddayMenuRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-    {
-        JSONParseResult json = JSON.Parse(Encoding.UTF8.GetString(body));
-        object Result = json.Result;
-
-        GD.Print(Result);
-
-        Array Results = Result as Array;
-        GD.Print(Results);
-
-        WednesdayMenu = Results;
-        EmitSignal("UpdatedWednesdayMenu");
-    }
-
-    public void _OnThudayMenuRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-    {
-        JSONParseResult json = JSON.Parse(Encoding.UTF8.GetString(body));
-        object Result = json.Result;
-
-        GD.Print(Result);
-
-        Array Results = Result as Array;
-        GD.Print(Results);
-
-        ThursdayMenu = Results;
-        EmitSignal("UpdatedThursdayMenu");
-    }
-
-    public void _OnFridayMenuRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-    {
-        JSONParseResult json = JSON.Parse(Encoding.UTF8.GetString(body));
-        object Result = json.Result;
-
-        GD.Print(Result);
-
-        Array Results = Result as Array;
-        GD.Print(Results);
-
-        FridayMenu = Results;
-        EmitSignal("UpdatedFridayMenu");
     }
 
 }
--- Scripts/Screens/MenuScreen.cs
+++ Scripts/Screens/MenuScreen.cs
@@ -1,392 +1,202 @@
-using Godot;
-using Dictionary = Godot.Collections.Dictionary;
-using Array = Godot.Collections.Array;
-using System.Text;
 using System;
-using System.Linq;
+using Godot;
+
+using Array = Godot.Collections.Array;
+using Dictionary = Godot.Collections.Dictionary;
 
 public class MenuScreen : Control
 {
+    private GlobalProcess GlobalProcess;
 
-	private GlobalProcess GlobalProcess;
-
-	private Firebase Firebase;
-
-	private Control BoxLeft;
-
-	private ScrollContainer BoxLeftScroll;
-
-	private VBoxContainer BoxLeftVBox;
-
-	private Control BoxRight;
-
-	private TabContainer TabContainer;
-
-	private const int DAY_MONDAY = 0,
-				DAY_TUESDAY = 1,
-				DAY_WEDNESDAY = 2,
-				DAY_THURSDAY = 3,
-				DAY_FRIDAY = 4;
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		InitNodes();
-		ConnectSignals();
-
-		Firebase.UpdateDishes();
-		Firebase.UpdateMenu(Firebase.MondayMenuRequest, Firebase.MON_REFERENCE);
-	}
-
-	private void InitNodes()
-	{
-		GlobalProcess = GetNode<GlobalProcess>("/root/GlobalProcess");
-		Firebase = GetNode<Firebase>("/root/Firebase");
-
-		BoxLeft = GetNode<Control>("BoxLeft");
-		BoxLeftScroll = BoxLeft.GetNode<ScrollContainer>("BoxLeftScroll");
-		BoxLeftVBox = BoxLeftScroll.GetNode<VBoxContainer>("BoxLeftVBox");
-
-		BoxRight = GetNode<Control>("BoxRight");
-		TabContainer = BoxRight.GetNode<TabContainer>("TabContainer");
-	}
-
-	private void ConnectSignals()
-	{
-		Firebase.Connect("UpdatedDishList", this, "UpdateDishList");
-		Firebase.Connect("UpdatedMondayMenu", this, "UpdateMondayMenu");
-		Firebase.Connect("UpdatedTuesdayMenu", this, "UpdateTuesdayMenu");
-		Firebase.Connect("UpdatedWednesdayMenu", this, "UpdateWednesdayMenu");
-		Firebase.Connect("UpdatedThursdayMenu", this, "UpdateThursdayMenu");
-		Firebase.Connect("UpdatedFridayMenu", this, "UpdateFridayMenu");
-	}
-
-	public override void _UnhandledInput(InputEvent evt)
-	{
-		switch (evt)
-		{
-			case InputEventKey keyEvent:
-				HandleInput();
-				break;
-			default:
-				break;
-		}
-	}
-
-	private void HandleInput()
-	{
-		if (Input.IsActionJustPressed("ui_select"))
-		{
-			Firebase.UpdateDishes();
-		}
-	}
-
-	public void UpdateDishList()
-	{
-		ClearDishList();
-
-		int Index = 0;
-
-		foreach (Dictionary entry in Firebase.Dishes)
-		{
-			PackedScene _DishContainer = GD.Load<PackedScene>("res://Scenes/UI/DishContainer.tscn");
-			DishContainer dishContainer = (DishContainer)_DishContainer.Instance();
-			BoxLeftVBox.AddChild(dishContainer);
-
-			dishContainer.Dish.Id = Index;
-			dishContainer.Dish.Title = (string)entry["titulo"];
-			dishContainer.Dish.Price = (float)entry["precio"];
-			Index++;
-		}
-
-		switch (TabContainer.CurrentTab)
-		{
-			case DAY_MONDAY:
-				RecheckDishContainers(Firebase.MondayMenu);
-				break;
-			case DAY_TUESDAY:
-				RecheckDishContainers(Firebase.TuesdayMenu);
-				break;
-			case DAY_WEDNESDAY:
-				RecheckDishContainers(Firebase.WednesdayMenu);
-				break;
-			case DAY_THURSDAY:
-				RecheckDishContainers(Firebase.ThursdayMenu);
-				break;
-			case DAY_FRIDAY:
-				RecheckDishContainers(Firebase.FridayMenu);
-				break;
-		}
-	}
-
-	private void ClearDishList()
-	{
-		foreach (DishContainer dishContainer in BoxLeftVBox.GetChildren())
-		{
-			BoxLeftVBox.RemoveChild(dishContainer);
-			dishContainer.QueueFree();
-		}
-	}
-
-	public void UpdateMondayMenu()
-	{
-		VBoxContainer Menu = TabContainer.GetNode<VBoxContainer>("MONDAY/ScrollContainer/VBoxContainer");
-
-		foreach (MenuDishContainer menuDishContainer in Menu.GetChildren())
-		{
-			Menu.RemoveChild(menuDishContainer);
-			menuDishContainer.QueueFree();
-		}
-
-		if (Firebase.MondayMenu != null)
-		{
-			foreach (object element in Firebase.MondayMenu)
-			{
-				PackedScene _MenuDishContainer = GD.Load<PackedScene>("res://Scenes/UI/MenuDishContainer.tscn");
-				MenuDishContainer menuDishContainer = (MenuDishContainer)_MenuDishContainer.Instance();
-				Menu.AddChild(menuDishContainer);
-
-				int Index = Int32.Parse((string)element);
-
-				Dictionary Dish = Firebase.Dishes[Index] as Dictionary;
-
-				menuDishContainer.Dish.Id = Index;
-				menuDishContainer.Dish.Title = (string)Dish["titulo"];
-				menuDishContainer.Dish.Price = (float)Dish["precio"];
-			}
-		}
-
-		RecheckDishContainers(Firebase.MondayMenu);
-	}
-
-	public void UpdateTuesdayMenu()
-	{
-		VBoxContainer Menu = TabContainer.GetNode<VBoxContainer>("TUESDAY/ScrollContainer/VBoxContainer");
-
-		foreach (MenuDishContainer menuDishContainer in Menu.GetChildren())
-		{
-			Menu.RemoveChild(menuDishContainer);
-			menuDishContainer.QueueFree();
-		}
-
-
-		if (Firebase.TuesdayMenu != null)
-		{
-			foreach (object element in Firebase.TuesdayMenu)
-			{
-				PackedScene _MenuDishContainer = GD.Load<PackedScene>("res://Scenes/UI/MenuDishContainer.tscn");
-				MenuDishContainer menuDishContainer = (MenuDishContainer)_MenuDishContainer.Instance();
-				Menu.AddChild(menuDishContainer);
-
-				int Index = Int32.Parse((string)element);
-
-				Dictionary Dish = Firebase.Dishes[Index] as Dictionary;
-
-				menuDishContainer.Dish.Id = Index;
-				menuDishContainer.Dish.Title = (string)Dish["titulo"];
-				menuDishContainer.Dish.Price = (float)Dish["precio"];
-			}
-		}
-
-		RecheckDishContainers(Firebase.TuesdayMenu);
-	}
-
-	public void UpdateWednesdayMenu()
-	{
-		VBoxContainer Menu = TabContainer.GetNode<VBoxContainer>("WEDNESDAY/ScrollContainer/VBoxContainer");
-
-		foreach (MenuDishContainer menuDishContainer in Menu.GetChildren())
-		{
-			Menu.RemoveChild(menuDishContainer);
-			menuDishContainer.QueueFree();
-		}
-
-
-		if (Firebase.WednesdayMenu != null)
-		{
-			foreach (object element in Firebase.WednesdayMenu)
-			{
-				PackedScene _MenuDishContainer = GD.Load<PackedScene>("res://Scenes/UI/MenuDishContainer.tscn");
-				MenuDishContainer menuDishContainer = (MenuDishContainer)_MenuDishContainer.Instance();
-				Menu.AddChild(menuDishContainer);
-
-				int Index = Int32.Parse((string)element);
-
-				Dictionary Dish = Firebase.Dishes[Index] as Dictionary;
-
-				menuDishContainer.Dish.Id = Index;
-				menuDishContainer.Dish.Title = (string)Dish["titulo"];
-				menuDishContainer.Dish.Price = (float)Dish["precio"];
-			}
-		}
-
-		RecheckDishContainers(Firebase.WednesdayMenu);
-	}
-
-	public void UpdateThursdayMenu()
-	{
-		VBoxContainer Menu = TabContainer.GetNode<VBoxContainer>("THURSDAY/ScrollContainer/VBoxContainer");
-
-		foreach (MenuDishContainer menuDishContainer in Menu.GetChildren())
-		{
-			Menu.RemoveChild(menuDishContainer);
-			menuDishContainer.QueueFree();
-		}
-
-
-		if (Firebase.ThursdayMenu != null)
-		{
-			foreach (object element in Firebase.ThursdayMenu)
-			{
-				PackedScene _MenuDishContainer = GD.Load<PackedScene>("res://Scenes/UI/MenuDishContainer.tscn");
-				MenuDishContainer menuDishContainer = (MenuDishContainer)_MenuDishContainer.Instance();
-				Menu.AddChild(menuDishContainer);
-
-				int Index = Int32.Parse((string)element);
-
-				Dictionary Dish = Firebase.Dishes[Index] as Dictionary;
-
-				menuDishContainer.Dish.Id = Index;
-				menuDishContainer.Dish.Title = (string)Dish["titulo"];
-				menuDishContainer.Dish.Price = (float)Dish["precio"];
-			}
-		}
-
-		RecheckDishContainers(Firebase.ThursdayMenu);
-	}
-
-	public void UpdateFridayMenu()
-	{
-		VBoxContainer Menu = TabContainer.GetNode<VBoxContainer>("FRIDAY/ScrollContainer/VBoxContainer");
-
-		foreach (MenuDishContainer menuDishContainer in Menu.GetChildren())
-		{
-			Menu.RemoveChild(menuDishContainer);
-			menuDishContainer.QueueFree();
-		}
-
-
-		if (Firebase.FridayMenu != null)
-		{
-			foreach (object element in Firebase.FridayMenu)
-			{
-				PackedScene _MenuDishContainer = GD.Load<PackedScene>("res://Scenes/UI/MenuDishContainer.tscn");
-				MenuDishContainer menuDishContainer = (MenuDishContainer)_MenuDishContainer.Instance();
-				Menu.AddChild(menuDishContainer);
-
-				int Index = Int32.Parse((string)element);
-
-				Dictionary Dish = Firebase.Dishes[Index] as Dictionary;
-
-				menuDishContainer.Dish.Id = Index;
-				menuDishContainer.Dish.Title = (string)Dish["titulo"];
-				menuDishContainer.Dish.Price = (float)Dish["precio"];
-			}
-		}
-
-		RecheckDishContainers(Firebase.FridayMenu);
-	}
-
-	private DishContainer GetDishContainer(int index)
-	{
-		foreach (DishContainer dishContainer in BoxLeftVBox.GetChildren())
-		{
-			if (dishContainer.Dish.Id == index)
-			{
-				return dishContainer;
-			}
-		}
-		return null;
-	}
-
-	private void RecheckDishContainers(Array menu)
-	{
-		foreach (DishContainer dishContainer in BoxLeftVBox.GetChildren())
-		{
-			dishContainer.GetNode<CheckBox>("CheckBox").Pressed = false;
-		}
-
-		foreach (DishContainer dishContainer in BoxLeftVBox.GetChildren())
-		{
-			if (menu != null)
-			{
-				if (menu.Contains(dishContainer.Dish.Id.ToString()))
-				{
-					dishContainer.GetNode<CheckBox>("CheckBox").Pressed = true;
-				}
-			}
-		}
-	}
-
-	private void UpdateTab(int tab)
-	{
-		switch (tab)
-		{
-			case DAY_MONDAY:
-				Firebase.UpdateMenu(Firebase.MondayMenuRequest, Firebase.MON_REFERENCE);
-				break;
-			case DAY_TUESDAY:
-				Firebase.UpdateMenu(Firebase.TuedayMenuRequest, Firebase.TUE_REFERENCE);
-				break;
-			case DAY_WEDNESDAY:
-				Firebase.UpdateMenu(Firebase.WeddayMenuRequest, Firebase.WED_REFERENCE);
-				break;
-			case DAY_THURSDAY:
-				Firebase.UpdateMenu(Firebase.ThudayMenuRequest, Firebase.THU_REFERENCE);
-				break;
-			case DAY_FRIDAY:
-				Firebase.UpdateMenu(Firebase.FridayMenuRequest, Firebase.FRI_REFERENCE);
-				break;
-		}
-	}
-
-	public void _OnFetchDishesButtonPressed()
-	{
-		Firebase.UpdateDishes();
-	}
-
-	public void _OnAddDishesButtonPressed()
-	{
-		string Reference = Firebase.MENU_REFERENCE;
-
-		switch (TabContainer.CurrentTab)
-		{
-			case DAY_MONDAY:
-				Reference += Firebase.MON_REFERENCE;
-				break;
-			case DAY_TUESDAY:
-				Reference += Firebase.TUE_REFERENCE;
-				break;
-			case DAY_WEDNESDAY:
-				Reference += Firebase.WED_REFERENCE;
-				break;
-			case DAY_THURSDAY:
-				Reference += Firebase.THU_REFERENCE;
-				break;
-			case DAY_FRIDAY:
-				Reference += Firebase.FRI_REFERENCE;
-				break;
-		}
-
-		Reference += Firebase.JSON_EXTENSION;
-
-		Array Dishes = new Array();
-
-		foreach (DishContainer dishContainer in BoxLeftVBox.GetChildren())
-		{
-			if (dishContainer.GetNode<CheckBox>("CheckBox").Pressed == true)
-			{
-				Dishes.Add(dishContainer.Dish.Id.ToString());
-			}
-		}
-
-		Firebase.MakePUTRequest(Reference, Dishes);
-		UpdateTab(TabContainer.CurrentTab);
-	}
-
-	public void _OnTabChanged(int tab)
-	{
-		UpdateTab(tab);
-	}
+    private Firebase Firebase;
 
+    private Control BoxLeft;
+
+    private ScrollContainer BoxLeftScroll;
+
+    private VBoxContainer BoxLeftVBox;
+
+    private Control BoxRight;
+
+    private TabContainer BoxRightTabContainer;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        InitNodes();
+        ConnectSignals();
+
+        Firebase.UpdateData();
+    }
+
+    private void InitNodes()
+    {
+        GlobalProcess = GetNode<GlobalProcess>("/root/GlobalProcess");
+        Firebase = GetNode<Firebase>("/root/Firebase");
+
+        BoxLeft = GetNode<Control>("BoxLeft");
+        BoxLeftScroll = BoxLeft.GetNode<ScrollContainer>("BoxLeftScroll");
+        BoxLeftVBox = BoxLeftScroll.GetNode<VBoxContainer>("BoxLeftVBox");
+
+        BoxRight = GetNode<Control>("BoxRight");
+        BoxRightTabContainer = BoxRight.GetNode<TabContainer>("TabContainer");
+    }
+
+    private void ConnectSignals()
+    {
+        Firebase.Connect("UpdatedData", this, "UpdateData");
+    }
+
+    public void _OnAddDishesButtonPressed()
+    {
+        Array dishes = new Array();
+
+        foreach (DishContainer dishContainer in BoxLeftVBox.GetChildren())
+        {
+            if (dishContainer.GetNode<CheckBox>("CheckBox").Pressed == true)
+            {
+                dishes.Add(dishContainer.Dish.Id.ToString());
+            }
+        }
+
+        Firebase.SetMenu(GetTabName(), dishes);
+        Firebase.UpdateData();
+    }
+
+    public void _OnTabChanged(int tab)
+    {
+        RecheckDishContainers();
+        Firebase.UpdateData();
+    }
+
+    public void UpdateData()
+    {
+        UpdateDishes();
+
+        UpdateDayMenu("monday");
+        UpdateDayMenu("tuesday");
+        UpdateDayMenu("wednesday");
+        UpdateDayMenu("thursday");
+        UpdateDayMenu("friday");
+    }
+
+    private void UpdateDishes()
+    {
+        ClearDishList();
+
+        int index = 0;
+
+        foreach (Dictionary entry in Firebase.Dishes)
+        {
+            PackedScene _dishContainer = GD.Load<PackedScene>("res://Scenes/UI/DishContainer.tscn");
+            DishContainer dishContainer = (DishContainer)_dishContainer.Instance();
+            BoxLeftVBox.AddChild(dishContainer);
+
+            dishContainer.Dish.Id = index;
+            dishContainer.Dish.Title = (string)entry["titulo"];
+            dishContainer.Dish.Price = (float)entry["precio"];
+            index++;
+        }
+
+        UpdateDayMenu(GetTabName());
+    }
+
+    private void UpdateDayMenu(string day)
+    {
+        VBoxContainer menuContainer = ClearMenuContainer(day);
+
+        Array dayMenu = Firebase.GetMenu(day);
+
+        if (dayMenu.Count > 0)
+        {
+            foreach (object element in dayMenu)
+            {
+                PackedScene _menuDishContainer = GD.Load<PackedScene>("res://Scenes/UI/MenuDishContainer.tscn");
+                MenuDishContainer menuDishContainer = (MenuDishContainer)_menuDishContainer.Instance();
+                menuContainer.AddChild(menuDishContainer);
+
+                int index = Int32.Parse((string)element);
+
+                Dictionary dish = Firebase.GetDish(index);
+
+                menuDishContainer.Dish.Id = index;
+                menuDishContainer.Dish.Title = (string)dish["titulo"];
+                menuDishContainer.Dish.Price = (float)dish["precio"];
+            }
+
+            RecheckDishContainers();
+        }
+    }
+
+    private void ClearDishList()
+    {
+        foreach (DishContainer dishContainer in BoxLeftVBox.GetChildren())
+        {
+            BoxLeftVBox.RemoveChild(dishContainer);
+            dishContainer.QueueFree();
+        }
+    }
+
+    private VBoxContainer ClearMenuContainer(string day)
+    {
+        VBoxContainer menuContainer =
+            BoxRightTabContainer.GetNode<VBoxContainer>(day.ToUpper() + "/ScrollContainer/VBoxContainer");
+
+        foreach (MenuDishContainer menuDishContainer in menuContainer.GetChildren())
+        {
+            menuContainer.RemoveChild(menuDishContainer);
+            menuDishContainer.QueueFree();
+        }
+
+        return menuContainer;
+    }
+
+    private void RecheckDishContainers()
+    {
+        Array dayMenu = Firebase.GetMenu(GetTabName());
+
+        foreach (CheckBox CheckBox in GetDishCheckBoxes())
+        {
+            CheckBox.Pressed = false;
+        }
+
+        if (dayMenu.Count > 0)
+        {
+            foreach (CheckBox CheckBox in GetDishCheckBoxes())
+            {
+                DishContainer dishContainer = CheckBox.GetParent() as DishContainer;
+                CheckBox.Pressed = dayMenu.Contains(dishContainer.Dish.Id.ToString());
+            }
+        }
+    }
+
+    private Array GetDishCheckBoxes()
+    {
+        Array dishContainers = BoxLeftVBox.GetChildren();
+        Array checkBoxes = new Array();
+
+        foreach (DishContainer dishContainer in dishContainers)
+        {
+            checkBoxes.Add(dishContainer.GetNode<CheckBox>("CheckBox"));
+        }
+
+        return checkBoxes;
+    }
+
+    private DishContainer GetDishContainer(int index)
+    {
+        foreach (DishContainer dishContainer in BoxLeftVBox.GetChildren())
+        {
+            if (dishContainer.Dish.Id == index)
+            {
+                return dishContainer;
+            }
+        }
+        return null;
+    }
+
+    private string GetTabName()
+    {
+        return BoxRightTabContainer.GetCurrentTabControl().Name;
+    }
 }
