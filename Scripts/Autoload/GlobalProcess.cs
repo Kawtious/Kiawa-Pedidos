f7b33a904a874035d0554cf5dd028a0c03e7dbc3
Kawtious <mewmewby2@gmail.com>
2022-12-01 13:38:17 -0700

    Let `_Input` handle input instead of `_UnhandledInput`

--- Scripts/Autoload/GlobalProcess.cs
+++ Scripts/Autoload/GlobalProcess.cs
@@ -21,49 +21,11 @@ public class GlobalProcess : Node
         ResourceDirector = GetNode<ResourceDirector>("/root/ResourceDirector");
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-    }
-
-    public override void _Notification(int what)
-    {
-    }
-
-    public override void _UnhandledInput(InputEvent evt)
-    {
-        switch (evt)
-        {
-            case InputEventKey keyEvent:
-                HandleInput();
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void HandleInput()
+    public override void _Input(InputEvent evt)
     {
         if (Input.IsActionJustPressed("change_window_mode"))
         {
             ResourceDirector.Preferences.SwitchFullscreen();
-        }
-
-        if (Input.IsActionJustPressed("pause"))
-        {
-            Paused = !Paused;
-            GetTree().Paused = Paused;
-
-            if (GetTree().Paused == true)
-            {
-                ResourceDirector.MenuAudioStream.Stream = (AudioStream)GlobalProcess.ResourceDirector.GetResource("pause_sound");
-            }
-            else
-            {
-                ResourceDirector.MenuAudioStream.Stream = (AudioStream)GlobalProcess.ResourceDirector.GetResource("unpause_sound");
-            }
-
-            ResourceDirector.MenuAudioStream.Play();
         }
 
         if (Input.IsActionJustPressed("quit"))
